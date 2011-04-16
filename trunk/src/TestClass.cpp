91464fa166fc179b0c126a509c6e0cfe7bfc9c32
jayjaybillings@gmail.com <jayjaybillings@gmail.com@6d054fa8-dcf3-fcc6-b8cd-667fea832f1a>
2011-04-16 20:02:06 +0000

    Initial import of Starscream build tree and build tree test files. There's no actual Starscream source code in here yet, but the build system is stable. ~JJB 20110416 16:01

---
+++ trunk/src/TestClass.cpp
@@ -0,0 +1,17 @@
+/*
+ * TestClass.cpp
+ *
+ *  Created on: Apr 16, 2011
+ *      Author: jaybilly
+ */
+
+#include <TestClass.h>
+
+TestClass::TestClass() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TestClass::~TestClass() {
+	// TODO Auto-generated destructor stub
+}
