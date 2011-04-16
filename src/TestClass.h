b0701f628f4a0a6090199302016aed56342096a9
Jay Jay Billings <jayjaybillings@users.noreply.github.com>
2011-04-16 20:02:06 +0000

    Initial import of Starscream build tree and build tree test files. There's no actual Starscream source code in here yet, but the build system is stable. ~JJB 20110416 16:01

---
+++ src/TestClass.h
@@ -0,0 +1,17 @@
+/*
+ * TestClass.h
+ *
+ *  Created on: Apr 16, 2011
+ *      Author: jaybilly
+ */
+
+#ifndef TESTCLASS_H_
+#define TESTCLASS_H_
+
+class TestClass {
+public:
+	TestClass();
+	virtual ~TestClass();
+};
+
+#endif /* TESTCLASS_H_ */
